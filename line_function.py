d8735abcca26af12e8873555785bf320dfd3db00
Ayman <azy4@duke.edu>
2022-02-04 14:16:12 -0500

    Restructured function to correctly test if point is on line and return Boolean. test function was changed accordingly to reflect this changes

--- line_function.py
+++ line_function.py
@@ -1,10 +1,16 @@
-import numpy
-
 def line_function_creation(p1,p2, p3):
     x1, y1 = p1
     x2, y2 = p2
     x3, y3 = p3
-    slope = y2 - y1/ (x2 -x1)
+    slope = (y2 - y1)/(x2 -x1)
+    print(slope)
     offset = y2 - (slope * y1)
-    return (x3 * slope) + offset
+    print(offset)
+    if y3 == (x3 * slope) + offset:
+        return True
+    else: 
+        return False
 
+#if __name__ == "__main__":
+#    answer = line_function_creation((2,4), (4,8), 6)
+#    print(answer)
--- test_line_function.py
+++ test_line_function.py
@@ -1,12 +1,12 @@
 import pytest
 
-@pytest.mark.parametrize("input_x, expected_y", [
-    [1, 5], 
-    [2, 10],
-    [3, 15]])
+@pytest.mark.parametrize("input_x1, input_x2, input_x3, expected_y", [
+    [(1,2), (2,4), (3,6),  True], 
+    [(2,4), (4,8), (6,12), True], 
+    ])
 
     
-def test_line_function(input_x, expected_y):
+def test_line_function(input_x1, input_x2, input_x3, expected_y):
     from line_function import line_function_creation
-    answer = line_function_creation(input_x)
+    answer = line_function_creation(input_x1, input_x2, input_x3)
     assert answer == expected_y
